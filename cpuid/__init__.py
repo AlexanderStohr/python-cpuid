d32aa57b98b522385d8041bd1a6b88f5d95d967b
Michael Mohr <akihana@gmail.com>
2015-08-08 22:30:21 -0700

    Add several new methods, fix import in __init__.py

--- cpuid/__init__.py
+++ cpuid/__init__.py
@@ -1,7 +1,11 @@
-from _methods import *
+from ._methods import *
 
 __all__ = [
     'cpuid',
     'vendor',
     'stepping_id',
+    'model',
+    'family',
+    'processor_type',
+    'brand_string',
 ]
--- cpuid/_methods.py
+++ cpuid/_methods.py
@@ -18,3 +18,28 @@ def vendor():
 
 def stepping_id():
     return _cpuid.cpuid(1)[0] & 0xf
+
+def model():
+    a = _cpuid.cpuid(1)[0]
+    model_number = (a >> 4) & 0xf
+    extended_model = (a >> 16) & 0xf
+    return (extended_model << 4) + model_number
+
+def family():
+    a = _cpuid.cpuid(1)[0]
+    family_code = (a >> 8) & 0xf
+    extended_family = (a >> 20) & 0xff
+    return extended_family + family_code
+
+def processor_type():
+    return (_cpuid.cpuid(1)[0] >> 12) & 0x3
+
+def brand_id():
+    return _cpuid.cpuid(1)[1] & 0xff
+
+def brand_string():
+    a = cpuid(EXTENDED_OFFSET)
+    if a[0] < (EXTENDED_OFFSET | 0x4):
+        raise NotImplementedError("Brand string is not supported by this CPU")
+    segments = [_struct.pack("IIII", *_cpuid.cpuid(EXTENDED_OFFSET | k)) for k in (0x2, 0x3, 0x4)]
+    return str(''.join([s.decode('utf-8').strip('\0') for s in segments]))
